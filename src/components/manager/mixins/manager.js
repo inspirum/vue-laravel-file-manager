0ee233feda8d2c04de404991ad516dd531d83f62
gary <gary@inspirum.cz>
2020-04-16 14:51:46 +0200

    Add multiselect support
    (cherry picked from commit 283ef8bf2ccf24383f349cdec7cbfe875b5f63bb)

--- src/components/manager/mixins/manager.js
+++ src/components/manager/mixins/manager.js
@@ -101,7 +101,7 @@ export default {
       const alreadySelected = this.selected[type].includes(path);
 
       // if pressed Ctrl -> multi select
-      if (event.ctrlKey) {
+      if (event.shiftKey) {
         if (!alreadySelected) {
           // add new selected item
           this.$store.commit(`fm/${this.manager}/setSelected`, { type, path });
@@ -112,7 +112,7 @@ export default {
       }
 
       // single select
-      if (!event.ctrlKey && !alreadySelected) this.$store.commit(`fm/${this.manager}/changeSelected`, { type, path });
+      if (!event.shiftKey && !alreadySelected) this.$store.commit(`fm/${this.manager}/changeSelected`, { type, path });
     },
 
     /**
